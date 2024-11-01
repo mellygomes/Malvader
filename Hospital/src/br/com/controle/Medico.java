8287c06c6a31bbf18ba5652348d2a2ec649f0188
Gabriel Novaes <80960360+Yukitte@users.noreply.github.com>
2024-11-01 15:20:43 -0300

    Add files via upload

---
+++ Hospital/src/br/com/controle/Medico.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+/**
+ *
+ * @author User
+ */
+public class Medico extends Hospital {
+   public String CRM;
+
+    public String getCRM() {
+        return CRM;
+    }
+
+    public void setCRM(String CRM) {
+        this.CRM = CRM;
+    }
+   
+   
+}
